8f60249a525563ea14c1ea6e74db2394ab6693ad
rletilly <ronan.letilly@gmail.com>
2020-01-24 18:25:44 +0100

    definitif

--- main_script.py
+++ main_script.py
@@ -1,8 +1,12 @@
-import os, random, string
+import os, random, string, struct
 
-def seed():
-    length = 256
-    chars = '01'
-    random.seed = (os.urandom(130))
-    return ''.join(random.choice(chars) for i in range(length))
-print(seed())
+length = 132
+chars = '01'
+random.seed = (os.urandom(256))
+seed_binaire = ''.join(random.choice(chars) for i in range(length))
+print(len(seed_binaire))
+print(seed_binaire)
+tab = [0]*12
+for j in range(0,12):
+    tab[j] = seed_binaire[j:j+11]
+    print(tab[j])
