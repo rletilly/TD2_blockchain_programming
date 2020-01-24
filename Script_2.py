c25692e2c6531bdd8d54cc0b437bc1ce106a3bfe
rletilly <ronan.letilly@gmail.com>
2020-01-24 18:04:58 +0100

    fct d'aléa
    fct d'aléa

--- Script_2.py
+++ Script_2.py
@@ -1,7 +1,12 @@
 import os, random, string
 
-length = 256
+length = 132
 chars = '01'
-random.seed = (os.urandom(700221))
-
-print (''.join(random.choice(chars) for i in range(length)))
+random.seed = (os.urandom(256))
+seed_binaire = ''.join(random.choice(chars) for i in range(length))
+print(len(seed_binaire))
+print(seed_binaire)
+tab = [0]*12
+for j in range(0,12):
+    tab[j] = seed_binaire[j:j+11]
+    print(tab[j])
