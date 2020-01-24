24a7cbfcf28099b0dabfaa104726da875f9d44c0
Tfrx1er <theophile.freixa@outlook.fr>
2020-01-24 18:17:57 +0100

    changements

--- script_1.py
+++ script_1.py
@@ -1,8 +1,19 @@
 import os, random, string
 
-def int_alea():
-    rand = os.urandom(1)
-    print(int.from_bytes(rand, byteorder='big', signed=False))
-    print(bin.from_bytes(rand, byteorder='big', signed=False))
+def seed():
+    length = 132
+    chars = '01'
+    random.seed = (os.urandom(130))
+    return ''.join(random.choice(chars) for i in range(length))
 
-int_alea()
+def decoupage_11(seed):
+    tab =[]
+    pack =""
+    for i in range(len(seed)):
+        pack = pack + seed[i]
+        if i+1 % 11 == 0 :
+            tab.append(pack)
+            pack = ""
+    return tab
+
+print(decoupage_11(seed()))
